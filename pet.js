8a320224f6644bfc80ab017422f2390a5df8a04e
roldhamk <roldhamk@gmail.com>
2020-02-13 12:17:55 +0000

    adding base files and README.md

---
+++ pet.js
@@ -0,0 +1,97 @@
+class Pet {
+    constructor(name){
+        this._name = name;
+        this._age = 8;
+        this._maxAge = 10;
+        this._hungerLevel = 18;
+        this._thirstLevel = 20;
+        this._isBored = false;
+        this._exerciseLevel = 0;
+        this._maxExerciseLevel = 20;
+        this._isAlive = true;
+    }
+    // GETTERS
+    get name(){
+        return this._name;
+    }
+    get age() {
+        return this._age;
+    }
+    get maxAge(){
+        return this._maxAge;
+    }
+    get hungerLevel(){
+        return this._hungerLevel;
+    }
+    get thirstLevel(){
+        return this._thirstLevel;
+    }
+    get isBored(){
+        return this._isBored;
+    }
+    get exerciseLevel(){
+        return this._exerciseLevel;
+    }
+    get maxExerciseLevel(){
+        return this._maxExerciseLevel;
+    }
+    get isAlive(){
+        return this._isAlive;
+    }
+    // METHODS
+    feed(){
+        if (this.hungerLevel <= 19) {
+            this._hungerLevel++;
+            console.log(`Yummy! Hunger level is now ${this.hungerLevel}.`)
+        } else {
+            console.log(`I'm not hungry! Hunger level is maxed out at ${this.hungerLevel}.`)
+        }
+    }
+    giveDrink(){
+
+    }
+    play(){
+        
+    }
+    exercise(){
+
+    }
+    levelUp() {
+        this._age++;
+        if (this.age > this.maxAge){
+            console.log(`Max age reached! ${this.name} has died...`)
+        } else if (this.age <= this.maxAge) {
+            console.log(`Happy birthday to me! I'm ${this.age} years old.`);
+        }
+    }
+    checkHunger(){
+        if (this.hungerLevel >= 0 && this.hungerLevel <=10) {
+            return `I'm hungry...feed me! Hunger level: ${this.hungerLevel}`
+        } else if (this.hungerLevel >= 11 && this.hungerLevel <= 15) {
+            return `I'm pretty full...but I could eat more! Hunger level: ${this.hungerLevel} `
+        } else {
+            return `I'm full, stop feeding me!`
+        }
+    }
+    checkThirst(){
+        if (this.thirstLevel >= 0 && this.thirstLevel <=10) {
+            return `I'm thirsty...give me a drink! Thirst level: ${this.thirstLevel}`
+        } else if (this.thirstLevel >= 11 && this.thirstLevel <= 15) {
+            return `I'm pretty hydrated...but I could drink more! Thirst level: ${this.thirstLevel} `
+        } else {
+            return `No more water please!`
+        }
+    }
+    checkExerciseLevel(){
+
+    }
+}
+
+const lefou = new Pet("Lefou"); // creating pet
+// console.log(lefou) // printing pet info to console
+// console.log(lefou.checkHunger()); // expect "I'm full!" to print
+// console.log(lefou.checkThirst()); // expect "No more water please!" to print
+lefou.levelUp();
+lefou.levelUp();
+lefou.levelUp();
+
